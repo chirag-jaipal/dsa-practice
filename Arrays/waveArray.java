548846a5e893d9e1bf0389bd4ee29481736e4d10
assembler-creates <chiragjaipal17@gmail.com>
2025-10-11 07:44:06 +0530

    feat: Add solution for Wave Array problem

---
+++ Arrays/waveArray.java
@@ -0,0 +1,25 @@
+public class waveArray {
+
+    public static void sortInWave(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i += 2) {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+
+    public static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        print(arr);
+        sortInWave(arr);
+        print(arr);
+    }
+}
